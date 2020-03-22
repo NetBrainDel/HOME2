75da1589adb54350b48423811239a12041ac91ce
NetBRAINdel <62387102+NetBRAINdel@users.noreply.github.com>
2020-03-22 18:41:20 +0300

    calc

--- src/com/company/CalculatorWithOperator.java
+++ src/com/company/CalculatorWithOperator.java
@@ -1,39 +1,43 @@
 package com.company;
 
+import java.util.Objects;
+
 public class CalculatorWithOperator {
 
-        int sum (int a,int b){
-            int q = a + b;
+
+
+        double sum (double a,double b){
+            double q = a + b;
             return q;
         }
-        int sub (int a,int b){
+       /* int sub (int a,int b){
             int w = a - b;
             return w;
-        }
-        double div (int a,int b){
-          int e = a / b;
+        }*/
+        double div (double a,double b){
+            double e = a / b;
           return e;
         }
-        int mul (int a,int b){
-            int r = a * b;
+        double mul (double a,double b){
+            double r = a * b;
             return r;
         }
-        int step (int a,int b){
-            int t=1;
+        double step (double a,double b){
+            double t=1;
             for (int i = 0; i < b; i++) {
               t = t * a;
             }
             return t;
         }
-         double abs(double a){
+        /* double abs(double a){
             double y = Math.abs(a);
             return y;
            }
          double kor(double a){
             double o = Math.sqrt(a);
             return o;
-           }
-
-
-
+           }*/
 }
+
+
+
